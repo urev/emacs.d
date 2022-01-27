e1f46cf59aa6b9a96212b9076aff2ec2111ac56b
aisv <aproc@qq.com>
2022-01-27 22:10:03 +0800

    添加：Emacs 配置文件.

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yasnippet-snippets zenburn-theme yasnippet which-key smartparens ox-hugo neotree hl-todo helm-projectile helm-ag go-eldoc flycheck evil doom-modeline company-go clang-format ccls beacon ag))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(line-number ((t (:background "black" :foreground "#6F6F6F")))))
